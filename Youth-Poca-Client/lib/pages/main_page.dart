aa0c09b9565e026b1684695b6fe9b75d7efc7dc1
Yeonoc <chlqhtjr752@kookmin.ac.kr>
2022-12-28 16:56:31 +0900

    backbone upload

--- Youth-Poca-Client/lib/pages/main_page.dart
+++ Youth-Poca-Client/lib/pages/main_page.dart
@@ -5,6 +5,8 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
